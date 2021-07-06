fc29c0bda4881495bf7706c3475ba0323405efb2
Seungho Lee <jimmylee871124@gmail.com>
2021-07-07 00:45:47 +0900

    [Initialization]Install SprintBoot with dependencies. - Spring Web, Spring Data JPA, OAuth2 Resource Server, MariaDB Driver

---
+++ src/main/kotlin/com/example/DevWiki/DevWikiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.DevWiki
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevWikiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevWikiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/DevWiki/DevWikiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.DevWiki
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevWikiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
