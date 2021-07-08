11fefb72bafae166313d56f8477771c28b8e7716
SeunghoLee <jimmylee871124@gmail.com>
2021-07-09 01:41:12 +0900

    [TestFeature]Dummy SignupControllerTest

---
+++ src/test/kotlin/com/example/DevWiki/auth/signup/SingupControllerTest.kt
@@ -0,0 +1,12 @@
+package com.example.DevWiki.auth.signup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SingupControllerTest {
+
+    @Test
+    fun contextLoads() {
+    }
+}
